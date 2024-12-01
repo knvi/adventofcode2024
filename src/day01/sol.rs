08f079eafe89b6e66dd6e254ff050582b6ec2959
knvi <32068559+knvi@users.noreply.github.com>
2024-12-01 10:42:03 +0000

    act smarter

--- src/day01/sol.rs
+++ src/day01/sol.rs
@@ -3,24 +3,24 @@ use std::{cmp::{max, min}, collections::HashMap};
 const INPUT: &str = include_str!("../../input/input_01.txt");
 
 pub fn run() {
-    let mut left: Vec<i64> = Vec::new();
-    let mut right: Vec<i64> = Vec::new();
-
-    for line in INPUT.lines() {
-        let pair = line.split_once("   ").unwrap(); // dumb 3 spaces
-        // println!("{} / {} is the pair", pair.0, pair.1);
-        left.push(pair.0.parse::<i64>().unwrap());
-        right.push(pair.1.parse::<i64>().unwrap());
-    }
+    
+    let (mut left, mut right): (Vec<i64>, Vec<i64>) = INPUT
+        .lines()
+        .map(|line| {
+            let pair = line.split_once("   ").unwrap();
+            (pair.0.parse::<i64>().unwrap(), pair.1.parse::<i64>().unwrap())
+        })
+        .unzip();
 
     left.sort();
     right.sort();
 
-    let mut dist = 0;
-
-    for i in 0..left.len() {
-        dist += max(left[i], right[i]) - min(left[i], right[i])
-    }
+    let dist: i64 = left
+        .clone()
+        .into_iter()
+        .enumerate()
+        .map(|(i, l)| max(l, right[i]) - min(l, right[i]))
+        .sum();
 
     println!("part 1 result: {dist}");
 
@@ -28,18 +28,18 @@ pub fn run() {
     
     // lets just do a hashmap for this ig
 
-    let mut similarity_map = HashMap::<i64, i64>::new();
-    let mut similarity_score: i64 = 0;
+    let similarity_map: HashMap<_, _> = right
+        .iter()
+        .fold(HashMap::new(), |mut map, &item| {
+            *map.entry(item).or_insert(0) += 1;
+            map
+        });
 
-    for i in right {
-        *similarity_map.entry(i).or_insert(0) += 1;
-    }
-
-    for i in left {
-        let score = similarity_map.get(&i).unwrap_or(&0);
-
-        similarity_score += score*i;
-    }
+    let similarity_score: i64 = left
+        .iter()
+        .map(|&item| item * similarity_map.get(&item).copied().unwrap_or(0))
+        .sum();
+        
 
     println!("part 2 results: {similarity_score}");
 
